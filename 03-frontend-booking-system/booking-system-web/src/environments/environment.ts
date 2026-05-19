386b989042be72de9c403ccc67be630d2865cc50
littlek4za <littlek4za@hotmail.com>
2026-05-19 22:03:26 +0800

    ChangeLog (20260519-2203):
    1) Update budget limit for frontend

--- 03-frontend-booking-system/booking-system-web/src/environments/environment.ts
+++ 03-frontend-booking-system/booking-system-web/src/environments/environment.ts
@@ -3,7 +3,7 @@ import { LogLevel } from "@core/model/log-level";
 
 export const environment = {
     production: true,
-    backendApiUrl:"http://localhost:8080/api",
+    backendApiUrl:"https://zero4-booking-system.onrender.com/api",
     logLevel: LogLevel.Error,
     captchaSiteKey: '6LdRM_AsAAAAACUDOSxU2jsCT_Li5FMwnoBuYzNZ',
 };
